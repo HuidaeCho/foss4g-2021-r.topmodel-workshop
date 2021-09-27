b59ce8558b20650169ccee0afed145888e26e652
Huidae Cho <grass4u@gmail.com>
2021-09-26 20:45:15 -0400

    Add sample_random.R

---
+++ data/config_random.R
@@ -0,0 +1,26 @@
+nruns <- 10000
+skip_c <- 365
+skip_v <- 365
+path_c <- list(params="params.txt",
+	       topidxstats="topidxstats.txt",
+	       input="input_c_pet.txt",
+	       output="output_c.txt",
+	       sim="sim_random",
+	       obs="obs_c.txt")
+path_v <- list(params="params.txt",
+	       topidxstats="topidxstats.txt",
+	       input="input_v_pet.txt",
+	       output="output_v.txt",
+	       obs="obs_v.txt")
+
+calc_nse <- function(obs, sim, skip=0){
+	if(skip > 0){
+		obs <- obs[-(1:skip)]
+		sim <- sim[-(1:skip)]
+	}
+	1-sum((sim-obs)^2)/sum((mean(obs)-obs)^2)
+}
+
+calc_obj <- function(obs, sim, skip=0){
+	1-calc_nse(obs, sim, skip)
+}
---
+++ scripts/sample_random.R
@@ -0,0 +1,24 @@
+#!/usr/bin/env Rscript
+source("config_random.R")
+source("../scripts/ispso.R")
+source("../scripts/read_write_rtopmodel.R")
+source("../scripts/run_rtopmodel.R")
+
+obs_c <- read.table(path_c$obs)[[1]]
+sim_c_fmt <- sprintf("%s/sim_c_%%0%dd.txt", path_c$sim, floor(log10(nruns))+1)
+
+obj <- c()
+run <- 0
+
+for(run in 1:nruns){
+	x <- runif(par.dim)
+	sim_c <- run_rtopmodel_x(x, path_c, TRUE)
+
+	write.table(sim_c, sprintf(sim_c_fmt, run), row.names=FALSE, col.names=FALSE)
+
+	obj[run] <- calc_obj(obs_c, sim_c, skip_c)
+	sink(sprintf("%s/obj.txt", path_c$sim), append=TRUE)
+	cat(obj[run], "\n")
+	sink()
+	printf("%d: %f\n", run, obj[run])
+}
